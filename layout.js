fa4d1b05dda9da8358a1d9be2fa7a00de5f92adc
atharvaberde2 <136217365+atharvaberde2@users.noreply.github.com>
2025-02-04 12:35:18 -0800

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,32 +1,7 @@
-
-import {Inter} from "next/font/google";
-import "./globals.css";
-import AuthWrapper from "@/providers/AuthProvider";
-import Navbar from "@/components/Navbar";
-import {Toaster} from "react-hot-toast";
-import TanStackQuery from "@/providers/TanStack";
-
-const inter = Inter({subsets: ["latin"]});
-
-export const metadata = {
-    title: "Disability Insurance",
-    description: "A tool to help individuals with disabilities",
-};
-
-export default function RootLayout({children}) {
+export default function Layout({ children }) {
     return (
-        <html lang="en">
-            <TanStackQuery>
-                <AuthWrapper>
-                    <body className={inter.className}>
-                    <Navbar />
-                    <div>
-                        {children}
-                    </div>
-                    <Toaster position="bottom-right"/>
-                    </body>
-                </AuthWrapper>
-            </TanStackQuery>
-        </html>
+        <div className="flex flex-col min-h-screen bg-blue-50 text-gray-900">
+            {children}
+        </div>
     );
 }
